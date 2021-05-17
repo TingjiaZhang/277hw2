3fd31d742fe0052129825364cb7eae1d043bf36d
TingjiaZhang <31323496+TingjiaZhang@users.noreply.github.com>
2021-05-17 22:12:37 +0800

    Add files via upload

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by 张廷嘉 on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
